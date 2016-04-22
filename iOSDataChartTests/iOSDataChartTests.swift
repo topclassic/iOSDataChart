3cb1971bd77e9f287357b14eca6713cc0cb1d56e
Chotipat Top <topz.classic@gmail.com>
2016-04-22 17:31:07 +0700

    Initial Commit

---
+++ iOSDataChartTests/iOSDataChartTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSDataChartTests.swift
+//  iOSDataChartTests
+//
+//  Created by Chotipat on 4/22/2559 BE.
+//  Copyright © 2559 Chotipat. All rights reserved.
+//
+
+import XCTest
+@testable import iOSDataChart
+
+class iOSDataChartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
